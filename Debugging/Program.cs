49d99f81a605c8c343f5d3bdd9e265e321531c98
Lucky Pathan <lckpathan@gmail.com>
2022-12-19 15:52:59 +0530

    19-12-22 - LAB

---
+++ Debugging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    class Employee
+    {
+        public void totalsalary(int sal,int bons)
+        {
+            Console.WriteLine("Total salalry is  --1" + (sal + bons));
+            Console.WriteLine("Total salalry is  --2" + (sal + bons));
+            Console.WriteLine("Total salalry is  --3" + (sal + bons));
+            Console.WriteLine("Total salalry is  --4" + (sal + bons));
+            Console.WriteLine("Total salalry is  --5" + (sal + bons));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ee = new Employee();
+            ee.totalsalary(25000, 4000);
+            Console.WriteLine("Hellow World!");
+            Console.WriteLine("Hai");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ File_Handling/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Handling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            textBox1.ForeColor = colorDialog1.Color;
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            textBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res = fontDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+                textBox1.Font = fontDialog1.Font;
+        }
+        FileStream fs;
+        StreamReader sr;
+        StreamWriter sw;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult res = saveFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                fs = new FileStream(filename, FileMode.Create);
+                sw = new StreamWriter(fs);
+                sw.Write(textBox1.Text);
+                sw.Flush();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                string filename2 = openFileDialog1.FileName;
+                fs = new FileStream(filename2, FileMode.Open);
+                sr = new StreamReader(fs);
+                textBox1.Text = sr.ReadToEnd();
+            }
+        }
+    }
+}
